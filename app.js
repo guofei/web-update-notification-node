9e81a785c15b1d338f9266b6cba7b090f9c85701
guofei <guofei1986@gmail.com>
2017-09-10 00:03:21 +0900

    feat: add package-lock.json

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -9,12 +8,6 @@ const articles = require('./routes/articles');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
