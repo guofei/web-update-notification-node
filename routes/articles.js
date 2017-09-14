c5e6bf796b855e4a7f784f5c6db0ce6fc69b4835
guofei <guofei1986@gmail.com>
2017-09-14 22:49:24 +0900

    fix: check if chrome crash

--- routes/articles.js
+++ routes/articles.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const { createBrowser, render } = require('../lib/crawler');
-
-let browser = null;
-createBrowser().then((b) => { browser = b; });
+const { render } = require('../lib/crawler');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   const { url } = req.query;
-  if (!url || !browser) {
+  if (!url) {
     res.status(400).json({ message: 'invalid' });
     return;
   }
-  render(browser, url).then((data) => {
+  render(url).then((data) => {
     res.json(data);
-  }).catch(() => {
-    res.status(400).json({ message: 'Problems parsing Article' });
+  }).catch((e) => {
+    res.status(400).json(e.message);
   });
 });
 
